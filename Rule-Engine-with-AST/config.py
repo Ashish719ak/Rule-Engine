58f80b4190c646ebe4f9e957d8cc311c5b559a50
Ashish719ak <98838522+Ashish719ak@users.noreply.github.com>
2024-10-26 09:31:20 +0530

    Delete Rule-Engine-with-AST directory

--- Rule-Engine-with-AST/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://root:root@db/rule_engine?charset=utf8mb4')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
